5ad1b451ac50ae3ec8e4f93873d3eabf3145e114
p7fw7x8sbm-blip <p7fw7x8sbm@privaterelay.appleid.com>
2025-11-21 09:17:26 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Business Dashboard with Streamlit Layouts")
+
+msg = "Objective: To demonstrate the usage of columns, tabs, and dynamic container in a business dashboard"
+st.write(msg)
